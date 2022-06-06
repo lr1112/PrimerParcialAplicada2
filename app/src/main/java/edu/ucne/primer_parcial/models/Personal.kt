d21043d06cee0a123a78474cedff3b48061f6535
lr1112 <luis_rosario_0324@hotmail.com>
2022-06-06 14:22:47 -0400

    creando entidad personal

--- app/src/main/java/edu/ucne/primer_parcial/models/Personal.kt
+++ app/src/main/java/edu/ucne/primer_parcial/models/Personal.kt
@@ -1,4 +1,16 @@
 package edu.ucne.primer_parcial.models
 
-class Personal {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class Personal(
+    @PrimaryKey(autoGenerate = true)
+    val personaId:Int,
+    val Nombre:String,
+    val Apellido:String,
+    val Telefono:String,
+    val Correo:String,
+    val ocupacion:String
+)
